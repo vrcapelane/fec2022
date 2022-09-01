c2d21073ddb40c0f48cb16850231702080edab0a
Vitor Regatieri Capelane <vitorregatiericapelane@gmail.com>
2022-08-31 23:34:46 -0300

    Correção taxa e resolução

--- FEC-2022-main/fec2022main.py
+++ FEC-2022-main/fec2022main.py
@@ -11,25 +11,27 @@ eyeClassifier = cv2.CascadeClassifier(eye_cascade)
 capture = cv2.VideoCapture(0)
 
 # definindo tamanho da imagem 
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
 # contadores
 contadorDeOlhosAux = 0
 contadorDeFacesAux = 0
-contadorDeFacesMax = 0
-contadorDeOlhosMax = 0
+
+# taxa de amostragem
+taxa = 64
 
 # logica de detecção
-while not cv2.waitKey(8) & 0xFF == ord("q"):
+while not cv2.waitKey(taxa) & 0xFF == ord("q"):
+
     ret, frame_color = capture.read()
 
     faces = faceClassifier.detectMultiScale(frame_color)
     eyes = eyeClassifier.detectMultiScale(frame_color)
 
     font = cv2.FONT_HERSHEY_DUPLEX
-    tamFont = 0.75
-    boldFont = 2
+    tamFont = 0.5
+    boldFont = 1
     corFace = (0, 202, 99)
     corEye = (0, 165, 255)
     corMensagemSair = (0, 0, 180)
@@ -38,29 +40,21 @@ while not cv2.waitKey(8) & 0xFF == ord("q"):
     contadorDeFaces = 0
     contadorDeOlhos = 0
 
-    cv2.putText(frame_color, 'Pressione "q" para fechar', (10, 25), font, tamFont, corMensagemSair, boldFont)
+    cv2.putText(frame_color, 'Pressione "q" para fechar', (5, 15), font, tamFont, corMensagemSair, boldFont)
 
     for x, y, w, h in faces:
-
         contadorDeFaces = contadorDeFaces + 1
         cv2.rectangle(frame_color, (x, y), (x + w, y + h), corFace, 2)
         cv2.putText(frame_color, 'Rosto', (x, y - 5), font, tamFont, corFace, boldFont)
-        cv2.putText(frame_color, 'Numero de faces encontradas: ' + str(contadorDeFacesAux) + ' (MAX: ' + str(contadorDeFacesMax) + ')', (10, 55), font, tamFont, corMsgContadores, boldFont)
+        cv2.putText(frame_color, 'Rostos encontrados: ' + str(contadorDeFacesAux), (5, 30), font, tamFont, corMsgContadores, boldFont)
 
     for x, y, w, h in eyes:
-
         contadorDeOlhos = contadorDeOlhos + 1
         cv2.rectangle(frame_color, (x, y), (x + w, y + h), corEye, 2)
         cv2.putText(frame_color, 'Olho', (x, y - 5), font, tamFont, corEye, boldFont)
-        cv2.putText(frame_color, 'Numero de olhos encontrados: ' + str(contadorDeOlhosAux) + ' (MAX: ' + str(contadorDeOlhosMax) + ')', (10, 85), font, tamFont, corMsgContadores, boldFont)
+        cv2.putText(frame_color, 'Olhos encontrados: ' + str(contadorDeOlhosAux), (5, 45), font, tamFont, corMsgContadores, boldFont)
 
     contadorDeOlhosAux = contadorDeOlhos
     contadorDeFacesAux = contadorDeFaces
-
-    if(contadorDeFacesMax < contadorDeFacesAux):
-        contadorDeFacesMax = contadorDeFacesAux
-
-    if(contadorDeOlhosMax < contadorDeOlhosAux):
-        contadorDeOlhosMax = contadorDeOlhosAux
 
     cv2.imshow('Fec2022-openCV', frame_color)
